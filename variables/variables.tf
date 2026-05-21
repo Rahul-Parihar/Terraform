18da422d716c2026d0565cf17b078e48f2c4827b
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2026-05-21 16:42:13 +0530

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,18 +1,17 @@
-variable " ec2_insatnce_name"{
-    description = "This variables holds EC2 instance name"
-    default = "terra-automate-server"
-    type = string
+variable "ec2_instance_name" {
+  description = "EC2 instance name"
+  type        = string
+  default     = "terra-automatic-server"
 }
 
-
-variable " ec2_volume-size"{
-    description = "This variables holds EC2 instance volume size"
-    default = 10
-    type = number
+variable "ec2_volume_size" {
+  description = "EC2 volume size"
+  type        = number
+  default     = 10
 }
 
-variable " aws_ec2_instance_state"{
-    description = "This variables holds EC2 instance state"
-    default = running
-    type = string
+variable "aws_ec2_instance_state" {
+  description = "EC2 instance state"
+  type        = string
+  default     = "running"
 }
