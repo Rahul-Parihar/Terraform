95b79fc1fdc9dba75473f196c2538ec026decd0e
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2026-05-21 16:23:04 +0530

    Create variables.tf

---
+++ variables/variables.tf
@@ -0,0 +1,12 @@
+variable " ec2_insatnce_name"{
+    description = "This variables holds EC2 instance name"
+    default = "terra-automate-server"
+    type = string
+}
+
+
+variable " ec2_volume-size"{
+    description = "This variables holds EC2 instance volume size"
+    default = 10
+    type = number
+}
