3dafadb26ac41d64f5bf65d2731b791fc04a0294
Rahul Singh Parihar <105512629+Rahul-Parihar@users.noreply.github.com>
2026-05-21 16:11:01 +0530

    Create ec2.tf

---
+++ terraform-advance/ec2.tf
@@ -0,0 +1,71 @@
+
+# Key Pair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "terra-automatic-key"
+  public_key = file("terraform.pub")
+}
+
+# Default VPC
+resource "aws_default_vpc" "default" {
+
+}
+
+# Security Group
+resource "aws_security_group" "my_security_group" {
+  name        = "terra-security-group"
+  description = "This is inbound and outbound rules for your instance"
+  vpc_id      = aws_default_vpc.default.id
+}
+
+# HTTP Rule
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# SSH Rule
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# Outbound Rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# EC2 Instance
+resource "aws_instance" "my_instance" {
+
+  ami           = "ami-07a00cf47dbbc844c"
+  instance_type = "t3.micro"
+
+  key_name = aws_key_pair.my_key_pair.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.my_security_group.id
+  ]
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "terra-automatic-server"
+  }
+}
+
+resource "aws_ec2_instance_state" "my_instance_state" {
+    instance_id = aws_instance.my_instance.id
+    state = "running"
+  
+}
